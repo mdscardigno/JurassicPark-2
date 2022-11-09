55f80787916def570b7783a78be2dfcb7848823b
Milagro Scardigno <mdscardigno@gmail.com>
2022-11-09 14:57:48 -0500

    additional notes

--- Dinosaur.cs
+++ Dinosaur.cs
@@ -1,7 +1,20 @@
+using System;
+
 namespace JurassicPark
 {
     public class Dinosaur
     {
-
+        public string Name { get; set; }
+        public string DietType { get; set; }
+        public DateTime WhenAcquired { get; set; }
+        public int Weight { get; set; }
+        public int EnclosureNumber { get; set; }
+        public string Description
+        {
+            get
+            {
+                return $"{Name} is a {DietType} dinosaur that weighs {Weight} pounds and was acquired on {WhenAcquired}. It is currently in enclosure {EnclosureNumber}.";
+            }
+        }
     }
 }
